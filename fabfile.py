f2f94c50366392d74d0fc3b729c256393c0cefc1
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2015-04-10 18:14:06 +0600

    Extend forms demo testcases

--- fabfile.py
+++ fabfile.py
@@ -8,7 +8,7 @@ BASEDIR = os.path.dirname(env.real_fabfile)
 @task
 def start_xvfb():
     local('/sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid'
-          '--make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x24')
+          '--make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16')
 
 
 @task
