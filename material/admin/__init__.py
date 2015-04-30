ad42c66676cc8b7778a4020fff3402bc100f212c
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2015-04-30 15:26:23 +0600

    Add module declaration for karenina

--- material/admin/__init__.py
+++ material/admin/__init__.py
@@ -1,1 +1,10 @@
 default_app_config = 'material.admin.apps.MaterialAdminConfig'
+
+
+try:
+    from . import modules
+    admin = modules.Admin()
+except ImportError:
+    """
+    Ok, karenina is not installed
+    """
