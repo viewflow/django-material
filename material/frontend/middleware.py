1b1a2d7aad2d2384b3154deb79d3b4b0489cd576
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2015-05-25 15:59:36 +0600

    Clear back if redirected on the same page

--- material/frontend/middleware.py
+++ material/frontend/middleware.py
@@ -1,5 +1,10 @@
-from urllib.parse import urlencode, parse_qs, urlsplit, urlunsplit
 from django.http import HttpResponseRedirect
+
+try:
+    from urllib.parse import urlencode, parse_qs, urlsplit, urlunsplit
+except ImportError:
+    from urlparse import parse_qs, urlsplit, urlunsplit
+    from urllib import urlencode
 
 
 class SmoothNavigationMiddleware(object):
@@ -7,11 +12,16 @@ class SmoothNavigationMiddleware(object):
         if isinstance(response, HttpResponseRedirect):
             back = request.GET.get('back')
             if back:
+                _, _, back_path, _, _ = urlsplit(back)
                 scheme, netloc, path, query_string, fragment = urlsplit(response['location'])
                 query_params = parse_qs(query_string)
-                if 'back' not in query_params:
+
+                if path == back_path:
+                    query_params.pop('back', None)
+                elif 'back' not in query_params:
                     query_params['back'] = [back]
-                    new_query_string = urlencode(query_params, doseq=True)
-                    response['location'] = urlunsplit((scheme, netloc, path, new_query_string, fragment))
+
+                new_query_string = urlencode(query_params, doseq=True)
+                response['location'] = urlunsplit((scheme, netloc, path, new_query_string, fragment))
 
         return response
--- tests/settings.py
+++ tests/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'material.frontend.middleware.SmoothNavigationMiddleware',
 )
 
 SITE_ID = 1
