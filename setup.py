ef6860a5de8eec72cf24d739ab024554da26b20b
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2015-05-29 18:04:26 +0600

    Hot fixes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='django-material',
-    version='0.4.0',
+    version='0.4.1',
     description='Material design for django forms and admin',
     license='BSD',
     author='Mikhail Podgurskiy',
