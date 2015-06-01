3491b9bbdc1177150ba345cca8dd263fe33c2dd4
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2015-06-01 17:50:15 +0600

    Frontend - add pjax loading indicator

--- material/frontend/static/material/frontend/js/frontend.js
+++ material/frontend/static/material/frontend/js/frontend.js
@@ -1,4 +1,7 @@
 $(document).pjax('a:not(.no-pjax)', 'main');
+$(document)
+  .on('pjax:start', function() { $('#load_indicator').fadeIn(200); })
+  .on('pjax:end',   function() { $('#load_indicator').fadeOut(200); })
 $(document).on('ready pjax:complete', function() {
     $(document).activeNavigation("#nav-mobile");
     $('.modal-trigger').leanModal();
