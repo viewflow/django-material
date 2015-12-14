e5abb887539b5369c65f53c549bf057e86610be7
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2015-12-14 11:56:18 +0600

    django SelectDateWidget support

---
+++ material/widgets.py
@@ -0,0 +1,78 @@
+import datetime
+
+from django.conf import settings
+from django.utils import formats, six
+from django.forms.widgets import Widget
+from django.utils.encoding import force_str
+
+
+class SelectDateWidget(Widget):
+    """
+    Wrapper around django.widgets.SelectDateWidget.
+
+    Provides api suitble for template-based rendering
+    """
+    def __init__(self, widget):
+        self.widget = widget
+
+    @property
+    def date_re(self):
+        if hasattr(self.widget, 'date_re'):
+            return self.widget.date_re
+        else:
+            # django 1.8
+            from django.forms.widgets.extra import DATE_RE
+            return DATE_RE
+
+    def split_value(self, value):
+        try:
+            year_val, month_val, day_val = value.year, value.month, value.day
+        except AttributeError:
+            year_val = month_val = day_val = None
+            if isinstance(value, six.string_types):
+                if settings.USE_L10N:
+                    try:
+                        input_format = format.get_format('DATE_INPUT_FORMATS')[0]
+                        v = datetime.datetime.strptime(force_str(value), input_format)
+                        year_val, month_val, day_val = v.year, v.month, v.day
+                    except ValueError:
+                        pass
+                else:
+                    match = self.re_date.match(value)
+                    if match:
+                        year_val, month_val, day_val = [int(v) for v in match.groups()]
+
+        return year_val, month_val, day_val
+
+    def parse_date_fmt(self):
+        fmt = formats.get_format('DATE_FORMAT')
+        escaped = False
+        for char in fmt:
+            if escaped:
+                escaped = False
+            elif char == '\\':
+                escaped = True
+            elif char in 'Yy':
+                yield 'year'
+            elif char in 'bEFMmNn':
+                yield 'month'
+            elif char in 'dj':
+                yield 'day'
+
+    def none_choice(self, none_value):
+        return [] if self.widget.is_required else [none_value]
+
+    def selects_data(self, value):
+        year_val, month_val, day_val = self.split_value(value)
+        year_choices = self.none_choice(self.widget.year_none_value) + [(i, i) for i in self.widget.years]
+        month_choices = self.none_choice(self.widget.month_none_value) + list(six.iteritems(self.widget.months))
+        day_choices = self.none_choice(self.widget.day_none_value) + [(i, i) for i in range(1, 32)]
+
+        data = {
+            'year': {'type': 'year', 'value': year_val, 'choices': year_choices},
+            'month': {'type': 'month', 'value': month_val, 'choices': month_choices},
+            'day': {'type': 'day', 'value': day_val, 'choices': day_choices},
+        }
+
+        for field in self.parse_date_fmt():
+            yield data[field]
---
+++ tests/test_widget_selectdate.py
@@ -0,0 +1,50 @@
+import json
+
+from django import forms
+from django.test.utils import override_settings
+from django_webtest import WebTest
+from . import build_test_urls
+
+try:
+    from django.forms.widgets import SelectDateWidget
+except ImportError:
+    # django 1.8
+    from django.forms.extras import SelectDateWidget
+
+
+class SelectForm(forms.Form):
+    test_field = forms.DateField(
+        widget=SelectDateWidget)
+    data_field = forms.BooleanField(required=False, widget=forms.HiddenInput, initial=True,
+                                    help_text='To produce non empty POST for empty test_field')
+
+
+@override_settings(ROOT_URLCONF=__name__)
+class Test(WebTest):
+    default_form = SelectForm
+
+    def test_default_usecase(self):
+        page = self.app.get(self.test_default_usecase.url)
+
+        self.assertIn('id="id_test_field_container"', page.body.decode('utf-8'))
+        self.assertIn('id="id_test_field_year"', page.body.decode('utf-8'))
+        self.assertIn('id="id_test_field_month"', page.body.decode('utf-8'))
+        self.assertIn('id="id_test_field_day"', page.body.decode('utf-8'))
+        # self.assertIn('data-test="Test Attr"', page.body.decode('utf-8'))
+
+        form = page.form
+        self.assertIn('test_field_year', form.fields)
+        self.assertIn('test_field_month', form.fields)
+        self.assertIn('test_field_day', form.fields)
+
+        form['test_field_year'] = '2015'
+        form['test_field_month'] = '1'
+        form['test_field_day'] = '13'
+        response = json.loads(form.submit().body.decode('utf-8'))
+
+        self.assertIn('cleaned_data', response)
+        self.assertIn('test_field', response['cleaned_data'])
+        self.assertEquals('2015-01-13', response['cleaned_data']['test_field'])
+
+
+urlpatterns = build_test_urls(Test)
