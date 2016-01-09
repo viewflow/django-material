4e21afba3a908f50ec92d3392f76ff950b315d86
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2016-01-09 16:01:44 +0600

    Speed up tests

--- tests/visual/__init__.py
+++ tests/visual/__init__.py
@@ -1,7 +1,11 @@
 import os
 import unittest
 
+from selenium import webdriver
 from needle.cases import NeedleTestCase
+from needle.driver import (NeedleFirefox, NeedleChrome, NeedleIe, NeedleOpera,
+                           NeedleSafari, NeedlePhantomJS)
+
 from pyvirtualdisplay import Display
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
@@ -29,6 +33,32 @@ class VisualTest(NeedleTestCase, StaticLiveServerTestCase):
         super(VisualTest, cls).tearDownClass()
         if cls.display is not None:
             cls.display.stop()
+
+    @classmethod
+    def get_web_driver(cls):
+        """
+        Returns the WebDriver instance to be used. Defaults to `NeedleFirefox()`.
+        Override this method if you'd like to control the logic for choosing
+        the proper WebDriver instance.
+        """
+        browser_name = os.environ.get('NEEDLE_BROWSER')
+        browser_map = {
+            'firefox': NeedleFirefox,
+            'chrome': NeedleChrome,
+            'ie': NeedleIe,
+            'opera': NeedleOpera,
+            'safari': NeedleSafari,
+            'phantomjs': NeedlePhantomJS,
+        }
+        browser_class = browser_map.get(browser_name, NeedleFirefox)
+        browser_kwargs = {}
+        if browser_class == NeedleFirefox:
+            profile = webdriver.FirefoxProfile()
+            profile.set_preference("browser.startup.homepage", "about:blank")
+            profile.set_preference("startup.homepage_welcome_url", "about:blank")
+            profile.set_preference("startup.homepage_welcome_url.additional", "about:blank")
+            browser_kwargs = {'firefox_profile': profile}
+        return browser_class(**browser_kwargs)
 
     def assertScreenshot(self, element_or_selector, file, threshold=0.02):
         super(VisualTest, self).assertScreenshot(element_or_selector, file, threshold=threshold)
