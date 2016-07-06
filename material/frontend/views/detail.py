f15abc7affa99003dd6b918347c2c27465ef2c9c
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2016-07-06 07:31:53 +0600

    Details view

--- material/frontend/views/detail.py
+++ material/frontend/views/detail.py
@@ -1,7 +1,44 @@
+from django.core.exceptions import PermissionDenied
+from django.db import models
 from django.views import generic
 
 
 class DetailModelView(generic.DetailView):
+    viewset = None
+
+    def get_object_data(self):
+        for field in self.object._meta.fields:
+            if isinstance(field, models.AutoField):
+                continue
+            elif field.auto_created:
+                continue
+            else:
+                choice_display_attr = "get_{}_display".format(field.get_attname())
+            if hasattr(self.object, choice_display_attr):
+                value = getattr(self.object, choice_display_attr)()
+            else:
+                value = getattr(self.object, field.get_attname())
+
+                if value is not None:
+                    yield (field.verbose_name.title(), value)
+
+    def has_object_permission(self, request, obj):
+        if self.viewset is not None:
+            return self.viewset.has_view_permission(request, obj)
+        raise NotImplementedError('Viewset is not provided')
+
+    def get_object(self):
+        obj = super(DetailModelView, self).get_object()
+        if not self.has_object_permission(self.request, obj):
+            raise PermissionDenied
+        return obj
+
+    def get_context_data(self, **kwargs):
+        if 'opts' not in kwargs:
+            kwargs['opts'] = self.model._meta
+        kwargs['object_data'] = self.get_object_data()
+        return super(DetailModelView, self).get_context_data(**kwargs)
+
     def get_template_names(self):
         if self.template_name is None:
             opts = self.model._meta
