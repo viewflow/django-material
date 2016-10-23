9a5bcdbcf0e4d605029c54493700620cd9e6f204
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2016-10-23 20:17:26 +0600

    CSS/Js cleanup

--- material/static/material/js/materialize.forms.js
+++ material/static/material/js/materialize.forms.js
@@ -1,55 +1,54 @@
 (function() {
-  function init_forms($container) {
-
+  function initForms ($container) {
     // Formsets
     // https://bitbucket.org/ionata/django-formset-js
     $('.formset-field').formset({
       animateForms: true,
-      newFormCallback: init_forms
-    });
-
+      newFormCallback: initForms
+    })
+    
     // Select
     // http://materializecss.com/forms.html#select
     $container
       .find('select')
       .not('.disabled')
       .not('.material-ignore')
-      .material_select();
-
+      .material_select()
+    
     // Date/DateTime/Time
     // https://github.com/xdan/datetimepicker
     $container
       .find('[data-form-control="date"]')
-      .each(function() {
+      .each(function () {
         $(this).datetimepicker({
           format: this.dataset.dateFormat,
           timepicker: false,
           mask: false,
           scrollInput: false
-        });
-      });
+        })
+      })
     $container
       .find('[data-form-control="time"]')
-      .each(function() {
+      .each(function () {
         $(this).datetimepicker({
           format: this.dataset.dateFormat,
           datepicker: false,
-          timepicker:true,
-          mask:false,
-          scrollInput:false
-        });
-      });
+          timepicker: true,
+          mask: false,
+          scrollInput: false
+        })
+      })
     $container.find('[data-form-control="datetime"]').each(
-      function() {
+      function () {
         $(this).datetimepicker({
           format: this.dataset.dateFormat,
           datepicker: true,
-          timepicker:true,
-          mask:false,
-          scrollInput:false
-        });
-      });
+          timepicker: true,
+          mask: false,
+          scrollInput: false
+        })
+      })
   }
 
-  $(document).on('ready turbolinks:load', init_forms($(document)));
-})();
+  $(document).on('ready turbolinks:load', function() { initForms($(document)) })
+})()
--- material/static/material/js/materialize.frontend.js
+++ material/static/material/js/materialize.frontend.js
@@ -1,27 +1,16 @@
-$(document).on("turbolinks:load", function() {
-
+/* global $ */
+$(document).on('turbolinks:load', function () {
   // Sidenav
   // http://materializecss.com/side-nav.html
   // https://github.com/noraesae/perfect-scrollbar#how-to-use
-  $(".button-collapse").sideNav();
-  $(document).activeNavigation("#slide-out");
-  $('#slide-out').perfectScrollbar();
+  $('.button-collapse').sideNav()
+  $(document).activeNavigation('#slide-out')
+  $('#slide-out').perfectScrollbar()
+})
 
-  // Dropdowns
-  // http://materializecss.com/dropdown.html
-  $(document)
-    .find('.dropdown-button.constrain_width')
-    .dropdown({hover: false, constrain_width: true});
-
-  $(document)
-    .find('.dropdown-button')
-    .not('.constrain_width')
-    .dropdown({hover: false, constrain_width: false});
-});
-
-$(document).on("turbolinks:before-cache", function() {
+$(document).on('turbolinks:before-cache', function () {
   // Sidenav
-  $('#sidenav-overlay').remove();
-  $('.drag-target').remove();
-  $('#slide-out').perfectScrollbar('destroy');
-});
+  $('#sidenav-overlay').remove()
+  $('.drag-target').remove()
+  $('#slide-out').perfectScrollbar('destroy')
+})
