586a0eb46783b22bb8af067dd4b43fea95b9e541
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2016-10-23 12:25:59 +0600

    Cleanup init js code

--- material/frontend/static/material/frontend/js/frontend.js
+++ material/frontend/static/material/frontend/js/frontend.js
@@ -1,18 +1,27 @@
 $(document).on("turbolinks:load", function() {
+
+  // Sidenav
+  // http://materializecss.com/side-nav.html
+  // https://github.com/noraesae/perfect-scrollbar#how-to-use
   $(".button-collapse").sideNav();
   $(document).activeNavigation("#slide-out");
   $('#slide-out').perfectScrollbar();
+
+  // Dropdowns
+  // http://materializecss.com/dropdown.html
+  $container
+    .find('.dropdown-button.constrain_width')
+    .dropdown({hover: false, constrain_width: true});
+
+  $container
+    .find('.dropdown-button')
+    .not('.constrain_width')
+    .dropdown({hover: false, constrain_width: false});
 });
 
 $(document).on("turbolinks:before-cache", function() {
+  // Sidenav
   $('#sidenav-overlay').remove();
   $('.drag-target').remove();
   $('#slide-out').perfectScrollbar('destroy');
 });
-
-/*
-$(document).on('submit', 'form[data-pjax-get]', function(event) {
-    event.preventDefault()
-    $.pjax({'container': 'main', 'url' :this.action + '?' + $(this).serialize()});
-})
-*/
---
+++ material/static/material/js/forms.js
@@ -0,0 +1,55 @@
+(function() {
+  function init_forms($container) {
+
+    // Formsets
+    // https://bitbucket.org/ionata/django-formset-js
+    $('.formset-field').formset({
+      animateForms: true,
+      newFormCallback: init_forms
+    });
+
+    // Select
+    // http://materializecss.com/forms.html#select
+    $container
+      .find('select')
+      .not('.disabled')
+      .not('.material-ignore')
+      .material_select();
+
+    // Date/DateTime/Time
+    // https://github.com/xdan/datetimepicker
+    $container
+      .find('[data-form-control="date"]')
+      .each(function() {
+        $(this).datetimepicker({
+          format: this.dataset.dateFormat,
+          timepicker: false,
+          mask: false,
+          scrollInput: false
+        });
+      });
+    $container
+      .find('[data-form-control="time"]')
+      .each(function() {
+        $(this).datetimepicker({
+          format: this.dataset.dateFormat,
+          datepicker: false,
+          timepicker:true,
+          mask:false,
+          scrollInput:false
+        });
+      });
+    $container.find('[data-form-control="datetime"]').each(
+      function() {
+        $(this).datetimepicker({
+          format: this.dataset.dateFormat,
+          datepicker: true,
+          timepicker:true,
+          mask:false,
+          scrollInput:false
+        });
+      });
+  }
+
+  $(document).on('ready turbolinks:load', init_forms($(document)));
+})();
