2c8332ec41fe99851838ed4162bbe618d6cdeb03
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2017-01-13 13:08:16 +0600

    Embed datalist class functionality into listview

--- material/frontend/views/list.py
+++ material/frontend/views/list.py
@@ -1,18 +1,135 @@
+from __future__ import unicode_literals
+
 import json
 
+from collections import OrderedDict
+
 from django.contrib.auth.decorators import login_required
+from django.core.exceptions import FieldDoesNotExist
 from django.core.exceptions import ImproperlyConfigured, PermissionDenied
 from django.core.urlresolvers import reverse
 from django.db.models.query import QuerySet
+from django.forms.forms import pretty_name
 from django.http import JsonResponse
 from django.utils import six
 from django.utils.decorators import method_decorator
+from django.utils.encoding import smart_text
+from django.utils.html import format_html
 from django.views.generic import View
 from django.views.generic.base import ContextMixin, TemplateResponseMixin
 
 
-from ..datalist import DataList
 from .. import forms
+
+
+def _get_attr_label(owner, attr_name):
+    attr = getattr(owner, attr_name)
+    if hasattr(attr, "short_description"):
+        return attr.short_description
+    elif isinstance(attr, property) and hasattr(attr, "fget"):
+        if hasattr(attr.fget, "short_description"):
+            return attr.fget.short_description
+        else:
+            return pretty_name(attr.fget.__name__)
+    elif callable(attr):
+        return "--" if attr.__name__ == "<lambda>" else pretty_name(attr.__name__)
+    else:
+        return attr_name
+
+
+class ModelField(object):
+    """Retrive a field value from the model.
+
+    Field verbose name would be use as a label.
+    """
+
+    def __init__(self, field):  # noqa D102
+        self.field = field
+
+    def get_value(self, obj):  # noqa D102
+        return getattr(obj, self.field.name)
+
+    @property
+    def label(self):  # noqa D102
+        try:
+            return self.field.verbose_name
+        except AttributeError:
+            # field is likely a ForeignObjectRel
+            return self.field.related_model._meta.verbose_name
+
+    @property
+    def orderable(self):  # noqa D102
+        return True
+
+
+class ModelAttr(object):
+    """Retrive attribute value from the model instance.
+
+    If model attribute is a callable, to get the value it would be
+    called without any arguments.
+    """
+
+    def __init__(self, model, name, label=None):  # noqa D102
+        self.model = model
+        self.name = name
+        self._label = label
+
+    def get_value(self, obj):  # noqa D102
+        attr = getattr(obj, self.name)
+        if callable(attr):
+            return attr()
+        return attr
+
+    @property
+    def label(self):  # noqa D102
+        if self._label:
+            return self._label
+        return _get_attr_label(self.model, self.name)
+
+    @property
+    def orderable(self):  # noqa D102
+        return False
+
+
+class DataSourceAttr(object):
+    """Retrieve attribute value from extenal data source.
+
+    Data source attribute could be a property or callable.
+
+    For a callable, to get the value it would be called with model
+    instance.
+    """
+
+    def __init__(self, data_source, name):  # noqa D102
+        self.data_source = data_source
+        self.name = name
+
+    def get_value(self, obj):  # noqa D102
+        attr = getattr(self.data_source, self.name)
+        if callable(attr):
+            return attr(obj)
+        return attr
+
+    @property
+    def label(self):
+        """Retrive the label for the data source attibute.
+
+        Label could be specifiend in `.short_description` or the
+        attribue name would be used.
+
+        Example::
+
+        class SampleDataSource(object):
+            def description(self, model):
+                return "Sample for {}".format(model)
+            description.short_description = "Model Description"
+
+        """
+        return _get_attr_label(self.data_source, self.name)
+
+    @property
+    def orderable(self):  # noqa D102
+        return False
 
 
 class ListModelView(ContextMixin, TemplateResponseMixin, View):
@@ -31,8 +148,6 @@ class ListModelView(ContextMixin, TemplateResponseMixin, View):
 
     :keyword datatable_config: Datatable default config overrides
 
-    :keyword datalist_class: Queryset to data adapter implementation
-
     :keyword list_display: List of fields to display
 
     :keyword list_display_links: List of fields form `list_display`
@@ -48,7 +163,6 @@ class ListModelView(ContextMixin, TemplateResponseMixin, View):
     datatable_config = None
     template_name_suffix = '_list'
 
-    datalist_class = DataList
     list_display = ('__str__', )
     list_display_links = ()
 
@@ -133,17 +247,6 @@ class ListModelView(ContextMixin, TemplateResponseMixin, View):
             # Use only the first item in list_display as link
             return list(list_display)[:1]
 
-    def create_datalist(self):
-        """Create data adapter instance."""
-        list_display = self.get_list_display()
-        list_display_links = self.get_list_display_links(list_display)
-        return self.datalist_class(
-            self.object_list,
-            data_sources=[self, self.viewset] if self.viewset else [self],
-            list_display=list_display,
-            list_display_links=list_display_links
-        )
-
     def get_queryset(self):
         """Retrive the view queryset."""
         if self.queryset is not None:
@@ -191,7 +294,7 @@ class ListModelView(ContextMixin, TemplateResponseMixin, View):
         config = self.datatable_default_config.copy()
         config['pageLength'] = self.paginate_by
         config['ajax']['url'] = self.request.path
-        config['columns'] = self.datalist.get_columns_def()
+        config['columns'] = self.get_columns_def()
         if self.datatable_config is not None:
             config.update(self.datatable_config)
         return config
@@ -205,8 +308,8 @@ class ListModelView(ContextMixin, TemplateResponseMixin, View):
         context = super(ListModelView, self).get_context_data(**kwargs)
         context.update({
             'datatable_config': json.dumps(self.get_datatable_config()),
-            'headers': self.datalist.get_headers_data(),
-            'data': self.datalist.get_data(0, self.paginate_by),
+            'headers': self.get_headers_data(),
+            'data': self.get_data(0, self.paginate_by),
         })
 
         return context
@@ -216,19 +319,65 @@ class ListModelView(ContextMixin, TemplateResponseMixin, View):
         context = self.get_context_data()
         return self.render_to_response(context)
 
-    def get_item_data(self, item):
-        """Row data post-processing.
+    def get_data_attr(self, attr_name):
+        """Data getter for an attribute.
 
-        Include link to object detail to `list_display_links` columns.
+        Data could comes from the model field or extrnal `data_source`
+        method call.
         """
-        opts = self.model._meta
+        opts = self.object_list.model._meta
+        try:
+            return ModelField(opts.get_field(attr_name))
+        except FieldDoesNotExist:
+            if attr_name == "__str__":
+                return ModelAttr(self.object_list.model, attr_name, opts.verbose_name)
+            else:
+                data_sources = [self, self.viewset] if self.viewset is not None else [self]
+                for data_source in data_sources:
+                    if hasattr(data_source, attr_name):
+                        return DataSourceAttr(data_source, attr_name)
 
-        result = {}
-        if self.has_view_permission(self.request, item):
-            result['view_url'] = reverse(
-                '{}:{}_detail'.format(opts.app_label, opts.model_name),
-                args=[item.pk])
-        return result
+        raise AttributeError("Unable to lookup '{}' on {}" .format(attr_name, self.object_list.model._meta.object_name))
+
+    def total(self):
+        """Total dataset size."""
+        return self.object_list.count()
+
+    def total_filtered(self):
+        """Dataset size with filter appllied."""
+        return self.object_list.count()
+
+    def get_columns_def(self):
+        """Returns columns definition for the datables js config."""
+        return [
+            {'data': field_name, 'orderable': self.get_data_attr(field_name).orderable}
+            for field_name in self.list_display
+        ]
+
+    def get_headers_data(self):
+        """Readable column titles."""
+        for field_name in self.list_display:
+            attr = self.get_data_attr(field_name)
+            yield field_name, attr.label
+
+    def get_data(self, start, length):
+        """Get a page for datatable."""
+        for item in self.object_list[start:start+length]:
+            columns = OrderedDict()
+            for n, field_name in enumerate(self.list_display):
+                attr = self.get_data_attr(field_name)
+                value = smart_text(attr.get_value(item))
+                if field_name in self.list_display_links:
+                    value = format_html('<a href="{}">{}</a>', self.get_item_url(item), value)
+                columns[field_name] = value
+            yield item, columns
+
+    def get_item_url(self, item):
+        """Link to object detail to `list_display_links` columns."""
+        opts = self.model._meta
+        return reverse(
+            '{}:{}_detail'.format(opts.app_label, opts.model_name),
+            args=[item.pk])
 
     def get_json_data(self, request, *args, **kwargs):
         """Return `JSONResponse` with data for datatable."""
@@ -240,14 +389,13 @@ class ListModelView(ContextMixin, TemplateResponseMixin, View):
         length = self.request_form.cleaned_data['length']
 
         result = []
-        for item, columns_data in self.datalist.get_data(start, length):
-            columns_data.update(self.get_item_data(item))
+        for item, columns_data in self.get_data(start, length):
             result.append(columns_data)
 
         data = {
             "draw": draw,
-            "recordsTotal": self.datalist.total(),
-            "recordsFiltered": self.datalist.total_filtered(),
+            "recordsTotal": self.total(),
+            "recordsFiltered": self.total_filtered(),
             "data": result
         }
 
@@ -259,9 +407,11 @@ class ListModelView(ContextMixin, TemplateResponseMixin, View):
         if not self.has_view_permission(self.request):
             raise PermissionDenied
 
+        if self.list_display_links is not None and self.list_display:
+            self.list_display_links = self.list_display[0]
+
         self.request_form = forms.DatatableRequestForm(request.GET)
         self.object_list = self.get_queryset()
-        self.datalist = self.create_datalist()
         if 'HTTP_DATATABLE' in request.META:
             handler = self.get_json_data
         elif request.method.lower() in self.http_method_names:
