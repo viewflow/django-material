e85e4ae6f7ad3ecad793de003dbbd9afd60c1535
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2017-05-04 13:30:52 +0600

    Update python deps

--- tests/settings.py
+++ tests/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 SITE_ID = 1
