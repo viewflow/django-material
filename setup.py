7f36077a4b8bc0b1430370bbfe6be365bf9fce05
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2018-01-10 18:42:31 +0600

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except IOError:
 
 setup(
     name='django-material',
-    version='1.2.0',
+    version='1.2.2',
     description='Material design for django forms and admin',
     license='BSD',
     author='Mikhail Podgurskiy',
