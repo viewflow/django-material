6ecc2d8ac4df0c64cab52663e812a8f7d0778a7b
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2018-01-19 13:49:52 +0600

    Smal fixes

--- tests/settings.py
+++ tests/settings.py
@@ -95,7 +95,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '..', '.media')
 
 MEDIA_URL = '/media/'
 
