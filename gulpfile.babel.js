9029818c9ff8d8694e70841ad5eb4938e10d6ce5
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2019-02-05 13:12:55 +0600

    Cleanup warnings

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,15 +1,16 @@
-import autoprefixer from 'autoprefixer'
-import babel from 'gulp-babel'
-import concat from 'gulp-concat'
+/* eslint-env node */
+import autoprefixer from 'autoprefixer';
+import babel from 'gulp-babel';
+import concat from 'gulp-concat';
 import cssnano from 'cssnano';
-import gulp from 'gulp'
-import merge from 'merge-stream'
-import postcss from 'gulp-postcss'
-import pump from 'pump'
-import sass from 'gulp-sass'
-import uglify from 'gulp-uglify-es'
+import gulp from 'gulp';
+import merge from 'merge-stream';
+import postcss from 'gulp-postcss';
+import pump from 'pump';
+import sass from 'gulp-sass';
+import uglify from 'gulp-uglify-es';
 
-var supportedBrowsers = [
+let supportedBrowsers = [
   'last 2 versions',
   'Chrome >= 50',
   'Firefox >= 46',
@@ -17,7 +18,7 @@ var supportedBrowsers = [
   'Safari >= 9',
   'ChromeAndroid >= 50',
   'FirefoxAndroid >= 46'
-]
+];
 
 gulp.task('3rdparty.fonts', () => {
   return merge(
@@ -25,12 +26,12 @@ gulp.task('3rdparty.fonts', () => {
       .pipe(gulp.dest('./material/static/material/fonts/material-design-icons/')),
     gulp.src('./node_modules/roboto-npm-webfont/full/**/*')
       .pipe(gulp.dest('./material/static/material/fonts/roboto/'))
-    )
+    );
   }
-)
+);
 
 gulp.task('3rdparty.js', () => {
-  var deps = [
+  let deps = [
     'node_modules/jquery/dist/jquery.js',
     'node_modules/datatables.net-fixedheader/js/dataTables.fixedHeader.js',
     'node_modules/datatables.net-responsive/js/dataTables.responsive.js',
@@ -39,26 +40,26 @@ gulp.task('3rdparty.js', () => {
     'node_modules/perfect-scrollbar/dist/js/perfect-scrollbar.jquery.js',
     'node_modules/turbolinks/dist/turbolinks.js',
     'node_modules/@webcomponents/custom-elements/custom-elements.min.js',
-  ]
+  ];
 
   return gulp.src(deps)
-    .pipe(gulp.dest('./material/static/material/js/'))
-})
+    .pipe(gulp.dest('./material/static/material/js/'));
+});
 
 gulp.task('3rdparty.css', () => {
-  var deps = [
+  let deps = [
     './node_modules/datatables.net-fixedheader-dt/css/fixedHeader.dataTables.css',
     './node_modules/datatables.net-responsive-dt/css/responsive.dataTables.css',
     './node_modules/jquery-datetimepicker/jquery.datetimepicker.css',
     './node_modules/perfect-scrollbar/dist/css/perfect-scrollbar.css'
-  ]
+  ];
   return gulp.src(deps)
     .pipe(gulp.dest('./material/static/material/css/'))
-})
+});
 
 
 gulp.task('components.js', () => {
-  var deps = [
+  let deps = [
     './node_modules/@webcomponents/custom-elements/src/native-shim.js',
     './material/components/collapsible/index.js',
     './material/components/dropdown/index.js',
@@ -80,13 +81,13 @@ gulp.task('components.js', () => {
 });
 
 gulp.task('materialize.js', () => {
-  var deps = [
-    'node_modules/materialize-css/dist/js/materialize.js'
-  ]
+  let deps = [
+    'node_modules/materialize-css/dist/js/materialize.js',
+  ];
 
   return gulp.src(deps)
-    .pipe(gulp.dest('./material/static/material/js/'))
-})
+    .pipe(gulp.dest('./material/static/material/js/'));
+});
 
 gulp.task('materialize.scss', () => {
   return gulp.src('./material/static/material/sass/*.scss')
@@ -97,13 +98,13 @@ gulp.task('materialize.scss', () => {
     ))
     .pipe(postcss([
       autoprefixer({
-        browsers: supportedBrowsers
-      })
+        browsers: supportedBrowsers,
+      }),
     ]))
     .pipe(gulp.dest(
       './material/static/material/css/'
-    ))
-})
+    ));
+});
 
 gulp.task('materialize.django.scss', () => {
   return gulp.src('./material/static/material/sass/*.scss')
@@ -114,16 +115,16 @@ gulp.task('materialize.django.scss', () => {
     ))
     .pipe(postcss([
       autoprefixer({
-        browsers: supportedBrowsers
-      })
+        browsers: supportedBrowsers,
+      }),
     ]))
     .pipe(gulp.dest(
       './material/static/material/css/'
-    ))
-})
+    ));
+});
 
 gulp.task('frontend.min.js', ['materialize.js', '3rdparty.js', 'components.js'], (cb) => {
-  var deps = [
+  let deps = [
     'material/static/material/js/turbolinks.js',
     'material/static/material/js/jquery.js',
     'material/static/material/js/jquery.dataTables.js',
@@ -136,16 +137,16 @@ gulp.task('frontend.min.js', ['materialize.js', '3rdparty.js', 'components.js'],
     'material/static/material/js/materialize.js',
     'material/static/material/js/custom-elements.min.js',
     'material/static/material/js/materialize.components.js',
-  ]
+  ];
   pump([
     gulp.src(deps),
     concat('materialize.frontend.min.js'),
     uglify(),
-    gulp.dest('material/static/material/js/')], cb)
-})
+    gulp.dest('material/static/material/js/')], cb);
+});
 
 gulp.task('frontend.min.css', ['3rdparty.css', 'materialize.scss', 'materialize.django.scss'], () => {
-  var deps = [
+  let deps = [
     'material/static/material/css/materialize.css',
     'material/static/material/css/materialize.forms.css',
     'material/static/material/css/materialize.frontend.css',
@@ -153,17 +154,15 @@ gulp.task('frontend.min.css', ['3rdparty.css', 'materialize.scss', 'materialize.
     'material/static/material/css/responsive.dataTables.css',
     'material/static/material/css/fixedHeader.dataTables.css',
     'material/static/material/css/perfect-scrollbar.css'
-  ]
+  ];
 
   return gulp.src(deps)
     .pipe(concat('materialize.frontend.min.css'))
     .pipe(postcss([
-      cssnano()
+      cssnano(),
     ]))
     .pipe(gulp.dest('material/static/material/css/'))
-})
-
-
+});
 
 gulp.task('default', [
   '3rdparty.fonts',
@@ -173,5 +172,5 @@ gulp.task('default', [
   'materialize.django.scss',
   'materialize.js',
   'frontend.min.js',
-  'frontend.min.css'
-])
+  'frontend.min.css',
+]);
