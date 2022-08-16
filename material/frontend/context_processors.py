6f61215263cfe02dc50e508514d1d23208e46d92
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2022-08-16 09:49:04 +0600

    Allow modules context processor works without request.user Fix #524

--- material/frontend/context_processors.py
+++ material/frontend/context_processors.py
@@ -3,16 +3,12 @@ from . import modules as modules_registry
 
 def modules(request):
     """Add current module and modules list to the template context."""
-    if not hasattr(request, 'user'):
-        raise ValueError('modules context processor requires "django.contrib.auth.context_processors.auth"'
-                         'to be in TEMPLATE_CONTEXT_PROCESSORS in your settings file.')
-
     module = None
 
     if request.resolver_match:
         module = getattr(request.resolver_match.url_name, 'module', None)
 
     return {
-        'modules': modules_registry.available_modules(request.user),
+        'modules': modules_registry.available_modules(request.user) if hasattr(request, 'user') else [],
         'current_module': module,
     }
