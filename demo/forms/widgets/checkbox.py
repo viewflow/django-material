fa1d8cb6177419a21bbd29bb1b1de51878dcf687
Mikhail Podgurskiy <kmmbvnr@gmail.com>
2025-04-08 19:43:19 +0500

    Enhance checkbox widget with color attribute and add subtitle to demo form.

--- demo/forms/widgets/checkbox.py
+++ demo/forms/widgets/checkbox.py
@@ -1,10 +1,14 @@
 from django import forms
 from material.forms.renderers import MaterialFormRenderer
 
+
 class CheckboxInputForm(forms.Form):
     field1 = forms.BooleanField(help_text="default", required=False)
     field2 = forms.BooleanField(help_text="initial value", initial=True)
     field3 = forms.BooleanField(help_text="disabled", disabled=True, initial=True)
-    field4 = forms.BooleanField(help_text="on a row with textfield")
-    
+    field4 = forms.BooleanField(
+        help_text="on a row with textfield",
+        widget=forms.CheckboxInput(attrs={"color": "secondary"}),
+    )
+
     default_renderer = MaterialFormRenderer()
